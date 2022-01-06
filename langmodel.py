92a65a06587832dc83bef08083d7201b619e05c3
AliMarashian <45737450+AliMarashian@users.noreply.github.com>
2022-01-06 23:51:28 +0330

    added confusionmatrix code
    81% accuracy

--- langmodel.py
+++ langmodel.py
@@ -3,12 +3,15 @@ import json
 import math
 import os
 from typing import Dict, List, Tuple
+import matplotlib.pyplot as plt
 
 import torch
 from torch.utils.data import DataLoader
 from datasets import load_dataset
 from multiprocess import set_start_method
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import (ConfusionMatrixDisplay, accuracy_score,
+                             classification_report)
 from tqdm import tqdm
 from transformers import AutoModelForSequenceClassification  # Has MLM head
 from transformers import (AutoConfig, AutoTokenizer,
@@ -48,8 +51,11 @@ class LMClassifier(Classifier):
 
         self.train_data = {}
         self.eval_data = {}
+        self.test_data = {}
         if args.train:
-            self.load_data(self.TRAIN_DATA_PATH)
+            self.load_train_data(self.TRAIN_DATA_PATH)
+        if args.test:
+            self.load_test_data(self.TRAIN_DATA_PATH)
         # Only fine tune the MLM head:
         if args.freeze_encoder:
             for param in self.model.base_model.parameters():
@@ -58,7 +64,7 @@ class LMClassifier(Classifier):
     def load(self, path: str):
         pass
 
-    def load_data(self, path: str):
+    def load_train_data(self, path: str):
         f_train = open(path + 'train.json', "r")
         self.train_data = json.loads(f_train.read())
         f_train.close()
@@ -67,13 +73,18 @@ class LMClassifier(Classifier):
         self.eval_data = json.loads(f_eval.read())
         f_eval.close()
 
+    def load_test_data(self, path: str):
+        f_test = open(path + 'test.json', "r")
+        self.test_data = json.loads(f_test.read())
+        f_test.close()
+
     def save(self, dir_path: str = None):
         dir_path = dir_path or self.MODEL_DIR
         self.config.save_pretrained(dir_path)
         self.tokenizer.save_pretrained(dir_path)
         self.model.save_pretrained(dir_path)
 
-    def create_dataset(self):
+    def create_dataset(self, data_):
 
         def in_one_string(poem):
             ss = ''
@@ -91,6 +102,10 @@ class LMClassifier(Classifier):
 
             return texts, labels
 
+        texts, labels = read_data(data_)
+        encodings = self.tokenizer(texts, truncation=True, padding=True)
+        return PoetDataset(encodings, labels)
+
         train_texts, train_labels = read_data(self.train_data)
         eval_texts, eval_labels = read_data(self.eval_data)
         train_encodings = self.tokenizer(train_texts, truncation=True, padding=True)
@@ -101,7 +116,19 @@ class LMClassifier(Classifier):
         return self.train_dataset, self.eval_dataset
 
     def classify(self, sent: str):
-        pass
+        poem = sent.split('/')
+
+        def in_one_string(poem):
+            ss = ''
+            for i in range(len(poem) - 1):
+                ss += (poem[i] + ' ، ')
+            ss += (poem[len(poem) - 1])
+            return ss
+
+        out = self.model(torch.tensor(self.tokenizer.encode(
+            in_one_string(sent))).int().to(self.DEVICE).unsqueeze(0))
+        #print(out['logits'])
+        return {'prediction': Classifier.POETS[out['logits'].argmax(dim=1)]}
 
     def evaluate(self, model, dataloader):
         model.eval()
@@ -119,8 +146,27 @@ class LMClassifier(Classifier):
             total_num += X1.shape[0]
         return score / total_num
 
+    def get_prediction(self, model, dataloader):
+        model.eval()
+        score = 0
+        total_num = 0
+        preds = torch.Tensor().to(self.DEVICE)
+        for batch_idx, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
+            X1 = batch['input_ids'].to(self.DEVICE)
+            # print(X1.shape)
+            y1 = batch['labels'].to(self.DEVICE)
+            pred = model(X1, attention_mask=batch['attention_mask'].to(self.DEVICE))
+            # print(pred)
+            pred_label = pred[0].argmax(dim=1)
+            preds = torch.cat((preds, pred_label), 0)
+            batch_score = sum(pred_label == y1)
+            score += batch_score.item()
+            total_num += X1.shape[0]
+        return preds, score / total_num
+
     def train(self, args):
-        train_dataset, eval_dataset = self.create_dataset()
+        train_dataset = self.create_dataset(self.train_data)
+        eval_dataset = self.create_dataset(self.eval_data)
         print('dataset creation done')
 
         self.model.train()
@@ -157,9 +203,21 @@ class LMClassifier(Classifier):
                 self.save()
 
     def test(self, args):
-
+        test_dataset = self.create_dataset(self.test_data)
         data_collator = DataCollatorWithPadding(self.tokenizer)
-        eval_dataloader = DataLoader(self.eval_dataset, batch_size=4, collate_fn=data_collator,
+        test_dataloader = DataLoader(test_dataset, batch_size=4, collate_fn=data_collator,
                                      shuffle=False)
+        preds, acc = self.get_prediction(self.model, test_dataloader)
+        print(f'Accuracy: {round(acc, 2)}')
 
-        print(self.evaluate(self.model, eval_dataloader))
+        ConfusionMatrixDisplay.from_predictions(
+            test_dataset.labels, preds.cpu(),
+            normalize='true',
+            display_labels=['Hafez', 'Khayyam', 'Ferdousi', 'Moulavi',
+                            'Nezami', 'Saadi', 'Parvin', 'Sanaie', 'Vahshi', 'Roudaki'],
+        )
+
+        plt.gcf().set_size_inches(10, 10)
+        plt.savefig(os.path.join(args.data_path, 'lm_confusion.png'), dpi=100)
+
+        print(classification_report(test_dataset.labels, preds.cpu()))
