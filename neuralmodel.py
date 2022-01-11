bf07a05a0f6d06975d8c2ef30ddaded814f36e64
AliMarashian <45737450+AliMarashian@users.noreply.github.com>
2022-01-11 21:17:40 +0330

    added test, and weighted the  classes in training

--- neuralmodel.py
+++ neuralmodel.py
@@ -11,8 +11,10 @@ import torch.optim as optim
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 from torchtext.legacy.data import BucketIterator, Field, TabularDataset
 from tqdm import tqdm
+from sklearn.utils import class_weight
 from transformers import AutoTokenizer
-
+from sklearn.metrics import (ConfusionMatrixDisplay, accuracy_score,
+                             classification_report)
 from classifier import Classifier
 
 
@@ -30,7 +32,7 @@ class LSTM(nn.Module):
                             bidirectional=True)
         self.drop = nn.Dropout(p=0.5)
 
-        self.fc1 = nn.Linear(2*dimension, 64)
+        self.fc1 = nn.Linear(2 * dimension, 64)
         self.fc2 = nn.Linear(64, 10)
 
     def forward(self, text, text_len):
@@ -62,12 +64,14 @@ class NeuralClassifier(Classifier):
             'cuda' if torch.cuda.is_available() else 'cpu')
         self.tokenizer = AutoTokenizer.from_pretrained(
             self.MODELS_DIR if args.load_model else self.MODEL_NAME)
+        self.train_labels = []
         self.read_data()
         self.model = LSTM(len(self.text_field.vocab))
         if args.load_model:
             self.load(self.MODELS_DIR)
         self.model.to(self.DEVICE)
         self.optimizer = optim.Adam(self.model.parameters(), lr=args.neural_lr)
+
 
     def load(self, path: str):
         self.model = torch.load(os.path.join(
@@ -88,7 +92,17 @@ class NeuralClassifier(Classifier):
         valid_running_loss = 0.0
         best_valid_acc = 0
         device = self.DEVICE
-        criterion = nn.CrossEntropyLoss()
+
+
+        class_weights = class_weight.compute_class_weight(
+            class_weight='balanced',
+            classes=np.unique(self.train_labels),
+            y=self.train_labels
+        )
+
+        class_weights = torch.FloatTensor(class_weights).cuda()
+
+        criterion = nn.CrossEntropyLoss(weight=class_weights)
         eval_freq = args.neural_eval_freq
 
         # training loop
@@ -109,7 +123,7 @@ class NeuralClassifier(Classifier):
                 running_loss += loss.item()
 
             # evaluation step
-            if epoch % eval_freq == eval_freq-1:
+            if epoch % eval_freq == eval_freq - 1:
                 correct, total = 0, 0
                 self.model.eval()
                 with torch.no_grad():
@@ -127,9 +141,9 @@ class NeuralClassifier(Classifier):
 
                 # evaluation
                 average_train_loss = (
-                    running_loss / len(self.train_loader)) / eval_freq
+                                             running_loss / len(self.train_loader)) / eval_freq
                 average_valid_loss = valid_running_loss / len(self.val_loader)
-                valid_acc = correct/total
+                valid_acc = correct / total
                 if valid_acc > best_valid_acc:
                     best_valid_acc = valid_acc
                     self.save(self.MODELS_DIR)  # save the better model
@@ -146,7 +160,36 @@ class NeuralClassifier(Classifier):
         print('Training finished')
 
     def test(self, args):
-        pass  # TODO
+        self.model.eval()
+        device = self.DEVICE
+        total, correct = 0, 0
+        preds = torch.Tensor().to(self.DEVICE)
+        true_labels = torch.Tensor().to(self.DEVICE)
+        with torch.no_grad():
+            for ((text, text_len), labels), _ in self.test_loader:
+                labels = labels.long().to(device)
+                text = text.to(device)
+                text_len = text_len.to(device)
+                output = self.model(text, text_len)
+                predictions = output.argmax(dim=1)
+                preds = torch.cat((preds, predictions), 0)
+                true_labels = torch.cat((true_labels, labels), 0)
+                total += len(labels)
+                correct += (predictions == labels).sum().cpu().item()
+
+        print(f'Accuracy: {round(correct / total, 2)}')
+
+        ConfusionMatrixDisplay.from_predictions(
+            true_labels.cpu(), preds.cpu(),
+            normalize='true',
+            display_labels=['Hafez', 'Khayyam', 'Ferdousi', 'Moulavi',
+                            'Nezami', 'Saadi', 'Parvin', 'Sanaie', 'Vahshi', 'Roudaki'],
+        )
+
+        plt.gcf().set_size_inches(10, 10)
+        plt.savefig(os.path.join(args.data_path, 'neural_confusion.png'), dpi=100)
+
+        print(classification_report(true_labels.cpu(), preds.cpu()))
 
     def read_stopwords(self, file_name: str = 'stop_words.txt'):
         stopwords = []
@@ -160,8 +203,14 @@ class NeuralClassifier(Classifier):
         '''
         for file_name in ['train', 'eval', 'test']:
             lst_dict = []
-            with open(os.path.join(self.DATA_PATH, file_name+'.json'), 'r', encoding='utf-8') as f:
+            with open(os.path.join(self.DATA_PATH, file_name + '.json'), 'r', encoding='utf-8') as f:
                 lst_dict = json.load(f)
+
+            if file_name == 'train':
+                self.train_labels = []
+                for i in range(len(lst_dict)):
+                    self.train_labels.append(int(lst_dict[i]['poet']))
+
 
             df = pd.DataFrame(lst_dict)
             max_words = 400  # 256?
@@ -170,7 +219,7 @@ class NeuralClassifier(Classifier):
                 '، '.join([self.clean(x) for x in mesras]).split(' ')[:max_words]))
             df = df.reindex(columns=['text', 'label'])
             df.to_csv(os.path.join(self.DATA_PATH,
-                      file_name+'.csv'), index=False)
+                                   file_name + '.csv'), index=False)
 
         # Fields
         label_field = Field(sequential=False, use_vocab=False,
@@ -182,7 +231,10 @@ class NeuralClassifier(Classifier):
 
         # TabularDataset
         train, val, test = TabularDataset.splits(
-            path=self.DATA_PATH, train='train.csv', validation='eval.csv', test='test.csv', format='CSV', fields=fields, skip_header=True)
+            path=self.DATA_PATH, train='train.csv', validation='eval.csv', test='test.csv', format='CSV', fields=fields,
+            skip_header=True)
+
+
         # Iterators
         self.train_loader = BucketIterator(train, batch_size=32, sort_key=lambda x: len(x.text),
                                            device=self.DEVICE, sort=True, sort_within_batch=True)
@@ -190,6 +242,7 @@ class NeuralClassifier(Classifier):
                                          device=self.DEVICE, sort=True, sort_within_batch=True)
         self.test_loader = BucketIterator(test, batch_size=32, sort_key=lambda x: len(x.text),
                                           device=self.DEVICE, sort=True, sort_within_batch=True)
+
 
         # Vocabulary
         self.text_field.build_vocab(train, min_freq=3)  # Save this and load it
